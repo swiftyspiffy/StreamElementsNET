22c055606a9d82229d3eb6f6c1e6e59b6a306328
LP <grofit1@yahoo.com>
2023-02-13 14:22:23 +0000

    Added model for handling unknown events
    This model will allow easier extension by end users for complex/simple custom events as it provides all the required data rather than just the name. Also moved the project type to be .net standard 2.0 which should be fine for most consumers but is more indicative of it being a library not an application itself.

---
+++ StreamElementsNET/StreamElementsNET/Models/Unknown/UnknownEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace StreamElementsNET.Models.Unknown
+{
+    public class UnknownEventArgs
+    {
+        public string Type { get; }
+        public JToken Data { get; }
+
+        public UnknownEventArgs(string type, JToken data)
+        {
+            Type = type;
+            Data = data;
+        }
+    }
+}
